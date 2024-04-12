8bedffc9602c01d655e3dfe3bfc841b1272d433a
rats159 <113955400+rats159@users.noreply.github.com>
2024-04-12 18:40:49 -0500

    Removed `delcare` keyword

--- src/Game.ts
+++ src/Game.ts
@@ -2,17 +2,15 @@ import { UUID } from "crypto";
 import { readFileSync } from "fs";
 
 export default class Game {
-   public declare readonly id: UUID;
-   public declare static words;
+   public readonly id: UUID;
+   //Adapted from https://github.com/dwyl/english-words/blob/master/words_alpha.txt
+   public static readonly words: string[] = readFileSync("words_alpha.txt", {
+      encoding: "utf8",
+   }).split("\n");
 
-   static {
-      //Adapted from https://github.com/dwyl/english-words/blob/master/words_alpha.txt
-      Game.words = readFileSync("words_alpha.txt", { encoding: "utf8" }).split(
-         "\n"
-      );
-   }
-
+   //TODO: Fix this! I don't know what broke it, but some words arent findable.
    static isValidWord(word: string): boolean {
+      //Binary search
       let left = 0;
       let right = Game.words.length - 1;
 
