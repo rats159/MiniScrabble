1ab37a8e7d7bf53482fc25225d6cbb29dba9765f
rats159 <113955400+rats159@users.noreply.github.com>
2024-04-13 19:53:32 -0500

    Draggable Tiles
    Tiles can be placed on the board, or rearranged on the rack.

--- static/script.js
+++ static/script.js
@@ -1,5 +1,84 @@
+"use strict";
 async function verify(word) {
-   return await fetch(`http://localhost:8080/api/verify?word=${word}`).then(
-      (data) => data.json()
+   return await fetch(`http://localhost:8080/api/verify?word=${word}`).then((data) => data.json());
+}
+
+/**
+ *
+ * @param {DOMRect} rect1
+ * @param {DOMRect} rect2
+ */
+function overlaps(rect1, rect2) {
+   return (
+      rect1.right > rect2.left && rect1.left <= rect2.right && rect1.bottom > rect2.top && rect1.top <= rect2.bottom
    );
 }
+
+/**
+ * @param {HTMLDivElement} cell
+ */
+
+function setupCell(cell) {
+   cell.onpointerdown = (event) => {
+      let x = event.clientX;
+      let y = event.clientY;
+
+      event.preventDefault();
+
+      cell.classList.add("dragging");
+      cell.classList.remove("played");
+
+      cell.style.top = y - cell.clientHeight / 2 + "px";
+      cell.style.left = x - cell.clientWidth / 2 + "px";
+      cell.style.gridArea = "";
+
+      document.body.appendChild(cell);
+
+      cell.onpointerup = () => {
+         document.onpointermove = null;
+
+         const cellBounds = cell.getBoundingClientRect();
+         const boardBounds = board.getBoundingClientRect();
+         const rackBounds = rack.getBoundingClientRect();
+
+         if (overlaps(cellBounds, boardBounds)) {
+            const CELL_COUNT = 9;
+            const boardTileSize = boardBounds.width / CELL_COUNT;
+            const rowIndex = Math.min(Math.max(Math.round((cellBounds.top - boardBounds.top) / boardTileSize), 0), 8);
+            const colIndex = Math.min(Math.max(Math.round((cellBounds.left - boardBounds.left) / boardTileSize), 0), 8);
+
+            cell.style.gridRow = rowIndex + 1;
+            cell.style.gridColumn = colIndex + 1;
+            tileBoard.appendChild(cell);
+            cell.classList.add("played");
+         } else if (overlaps(cellBounds, rackBounds)) {
+            //Reorder rack
+            for (const rackTile of document.querySelectorAll(".cell:not(.dragging)")) {
+               if (rackTile.getBoundingClientRect().left > cellBounds.left) {
+                  rackTile.insertAdjacentElement("beforebegin", cell);
+                  break;
+               }
+            }
+         } else {
+            rack.appendChild(cell);
+         }
+         cell.classList.remove("dragging");
+         cell.style.top = "0px";
+         cell.style.left = "0px";
+      };
+
+      document.onpointermove = (event) => {
+         event.preventDefault();
+         x = event.clientX;
+         y = event.clientY;
+
+         cell.style.top = y - cell.clientHeight / 2 + "px";
+         cell.style.left = x - cell.clientWidth / 2 + "px";
+      };
+   };
+}
+
+document.querySelectorAll(".cell").forEach((cell) => setupCell(cell));
+const board = document.querySelector(".board");
+const rack = document.querySelector(".rack");
+const tileBoard = document.querySelector("#playedtiles");
