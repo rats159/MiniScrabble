603dfe8bfae41e50453fc92d22be17757e014780
rats159 <113955400+rats159@users.noreply.github.com>
2024-04-15 17:53:01 -0500

    Added functionality to rack utils

---
+++ static/buttons.js
@@ -0,0 +1,39 @@
+const [shuffle, sort, submit, exchange, pass] = document.querySelectorAll(".action");
+
+shuffle.addEventListener("click", () => {
+   const children = [...rack.children];
+   for (let i = 0; i < children.length; i++) {
+      const swapIndex = Math.floor(Math.random() * (children.length - i) + i);
+      const temp = children[swapIndex];
+      children[swapIndex] = children[i];
+      children[i] = temp;
+   }
+
+   for (const child of children) {
+      child.classList.add("shrinking");
+      setTimeout(() => {
+         rack.appendChild(child);
+         child.classList.add("growing");
+      }, 250);
+      setTimeout(() => {
+         child.classList.remove("shrinking", "growing");
+      }, 500);
+   }
+});
+
+sort.addEventListener("click", () => {
+   const children = [...rack.children];
+
+   children.sort((a, b) => a.dataset["letter"].charCodeAt(0) - b.dataset["letter"].charCodeAt(0));
+
+   for (const child of children) {
+      child.classList.add("sorting1");
+      setTimeout(() => {
+         rack.appendChild(child);
+         child.classList.add("sorting2");
+      }, 250);
+      setTimeout(() => {
+         child.classList.remove("sorting1", "sorting2");
+      }, 500);
+   }
+});
