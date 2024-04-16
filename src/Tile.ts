54014eeaa8309b3944746c56eda02fe69b402599
rats159 <113955400+rats159@users.noreply.github.com>
2024-04-15 19:57:08 -0500

    Added Tile.get() method

--- src/Tile.ts
+++ src/Tile.ts
@@ -34,4 +34,63 @@ export class Tile {
       this.letter = letter;
       this.score = score;
    }
+
+   public static get(letter: string): Tile {
+      switch (letter.toUpperCase()) {
+         case "A":
+            return Tile.A;
+         case "B":
+            return Tile.B;
+         case "C":
+            return Tile.C;
+         case "D":
+            return Tile.D;
+         case "E":
+            return Tile.E;
+         case "F":
+            return Tile.F;
+         case "G":
+            return Tile.G;
+         case "H":
+            return Tile.H;
+         case "I":
+            return Tile.I;
+         case "J":
+            return Tile.J;
+         case "K":
+            return Tile.K;
+         case "L":
+            return Tile.L;
+         case "M":
+            return Tile.M;
+         case "N":
+            return Tile.N;
+         case "O":
+            return Tile.O;
+         case "P":
+            return Tile.P;
+         case "Q":
+            return Tile.Q;
+         case "R":
+            return Tile.R;
+         case "S":
+            return Tile.S;
+         case "T":
+            return Tile.T;
+         case "U":
+            return Tile.U;
+         case "V":
+            return Tile.V;
+         case "W":
+            return Tile.W;
+         case "X":
+            return Tile.X;
+         case "Y":
+            return Tile.Y;
+         case "Z":
+            return Tile.Z;
+         case " ":
+            return Tile.BLANK;
+      }
+   }
 }
